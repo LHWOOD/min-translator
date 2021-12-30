8667f3ce24cfd0410fdf0e09ae05918eb561347f
LHWOOD <hutsonwood@gmail.com>
2021-12-30 14:12:27 -0700

    tested api and output format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,37 @@
-let yo = "yoyo";
+const userForm = document.querySelector("#submitForm");
+const symbolic = document.querySelector("#trans");
+const spot = document.querySelector("#stuff");
+// const button = document.querySelector("#btn");
 
-console.log(yo);
+const inputHandler = function (event) {
+  event.preventDefault();
+  let stock = symbolic.value.trim().toUpperCase();
+  console.log(stock);
+  getStock(stock);
+};
+
+function getStock(stock) {
+  const requestUrl =
+    "https://api.funtranslations.com/translate/minion.json?text=" + stock + "";
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (yo) {
+      console.log(yo);
+
+      let low = document.createElement("h1");
+
+      low.textContent = yo.contents.translated;
+
+      low.setAttribute("style", "color: #f4dd4b");
+      low.classList.add("min");
+
+      spot.appendChild(low);
+
+      document.querySelector("#submitForm").reset();
+    });
+}
+
+userForm.addEventListener("submit", inputHandler);
